f1bd4c783db0f03432720fbaad5cc75fb5185091
Jeffery Pereira <jpereira1@mail.csuchico.edu>
2017-02-27 12:36:44 -0800

    Setting up x06 repo properly with proper files

---
+++ TicTacToeBoardTest.cpp
@@ -0,0 +1,26 @@
+/**
+ * Unit Tests for TicTacToeBoard
+**/
+
+#include <gtest/gtest.h>
+#include "TicTacToeBoard.h"
+ 
+class TicTacToeBoardTest : public ::testing::Test
+{
+	protected:
+		TicTacToeBoardTest(){} //constructor runs before each test
+		virtual ~TicTacToeBoardTest(){} //destructor cleans up after tests
+		virtual void SetUp(){
+			
+		} //sets up before each test (after constructor)
+		virtual void TearDown(){} //clean up after each test, (before destructor) 
+};
+
+TEST(TicTacToeBoardTest, sanityCheck)
+{
+	TicTacToeBoard tttb; 
+	Piece p = tttb.getPiece(2, 3); 
+	ASSERT_TRUE(X == p || 0 == p || Invalid == p ||Blank == p);
+}
+
+
